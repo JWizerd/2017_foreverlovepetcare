7988cdce1fd76a0b4af2d19721c274b8f104f6e5
JWizerd <jwodke@outlook.com>
2017-02-08 13:19:59 -0700

    facebook ajax request now returning proper data

--- _site/assets/javascripts/main.js
+++ _site/assets/javascripts/main.js
@@ -1,1 +1,14 @@
+  var openGraphUrl = "https://graph.facebook.com/v2.8/";
+  var albumID = "936209046430553";
+  var token = "EAADPAHttTCwBAJbXlZBZA23OFbJrzDG9nnMlGfHQqca8APtyDN6l7jTEPB7p80ammNqZBj1wJJTHjAaOKcp40aGWieB2jaoQv73POUH6mH5KrkoMOAUl6mBcXj2IECeIPYnZBbCB1budzzKCCb0hHIoWFzAAWZCQru6mIBlWIsDHMXO6fGgo6Jw8CPLxZCOVYZD";
+  var limit = "30";
 
+  var endpoint = openGraphUrl + "/" + albumID + "/photos?access_token=" + token + "&limit=" + limit + "&field=url";
+
+  $.getJSON(endpoint, function(data) {
+      for( var i = 0; i < data.data.length; i++ ){
+        $('.fb-album-container').append("<div>" + data.data[i].id + "</div>");
+      }
+  }).fail(function() {
+    alert('Could not receive data. Please contact admin.');
+  });
