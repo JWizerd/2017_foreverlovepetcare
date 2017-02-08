8075f78e5030c6e23f4a8cda022ed15fb71c9d93
JWizerd <jwodke@outlook.com>
2017-02-08 14:33:38 -0700

    added media queries for clients gallery

--- _site/assets/javascripts/main.js
+++ _site/assets/javascripts/main.js
@@ -1,6 +1,6 @@
   var openGraphUrl = "https://graph.facebook.com/v2.8/";
   var albumID = "936209046430553";
-  var token = "EAADPAHttTCwBAJbXlZBZA23OFbJrzDG9nnMlGfHQqca8APtyDN6l7jTEPB7p80ammNqZBj1wJJTHjAaOKcp40aGWieB2jaoQv73POUH6mH5KrkoMOAUl6mBcXj2IECeIPYnZBbCB1budzzKCCb0hHIoWFzAAWZCQru6mIBlWIsDHMXO6fGgo6Jw8CPLxZCOVYZD";
+  var token = "EAADPAHttTCwBABoL3ypQ0lZBs2K4QZBeFCivMErpXjLsxikQfPMkldHZC8VzVVTcTaDqpchwhwpsnRhvZBssw7L7wkVJItHEcw59gVYDDFJZBicMkWRgZCAk9R2poSXup2ZCY3TGZCE2JGOw75HXDOEcgtKAH8g0e5EZD";
   var limit = "30";
 
   var endpoint = openGraphUrl + "/" + albumID + "/photos?access_token=" + token + "&fields=images&limit=" + limit;
@@ -8,9 +8,9 @@
   $.getJSON(endpoint, function(data) {
       for( var i = 0; i < data.data.length; i++ ){
         $('.fb-album-container').append(
-        '<div class="col-sm-3 col-xs-2"><img class="thumbnail" src="'
+        '<div class="col-md-2 col-sm-3 col-xs-4 facebook-photo-wrapper"><img class="thumbnail facebook-photo" src="'
           + data.data[i].images[4].source +
-        '"></div>"');
+        '"></div>');
       }
   }).fail(function() {
     alert('Could not receive data. Please contact admin.');
