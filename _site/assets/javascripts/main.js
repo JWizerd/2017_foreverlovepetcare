b0307bbc18e4e6b7be7343248d6d13a772ae485d
JWizerd <jwodke@outlook.com>
2017-02-08 13:49:13 -0700

    images are successfully updating via ajax to the webpage

--- _site/assets/javascripts/main.js
+++ _site/assets/javascripts/main.js
@@ -3,11 +3,14 @@
   var token = "EAADPAHttTCwBAJbXlZBZA23OFbJrzDG9nnMlGfHQqca8APtyDN6l7jTEPB7p80ammNqZBj1wJJTHjAaOKcp40aGWieB2jaoQv73POUH6mH5KrkoMOAUl6mBcXj2IECeIPYnZBbCB1budzzKCCb0hHIoWFzAAWZCQru6mIBlWIsDHMXO6fGgo6Jw8CPLxZCOVYZD";
   var limit = "30";
 
-  var endpoint = openGraphUrl + "/" + albumID + "/photos?access_token=" + token + "&limit=" + limit + "&field=url";
+  var endpoint = openGraphUrl + "/" + albumID + "/photos?access_token=" + token + "&fields=images&limit=" + limit;
 
   $.getJSON(endpoint, function(data) {
       for( var i = 0; i < data.data.length; i++ ){
-        $('.fb-album-container').append("<div>" + data.data[i].id + "</div>");
+        $('.fb-album-container').append(
+        '<div class="col-sm-3 col-xs-2"><img class="thumbnail" src="'
+          + data.data[i].images[4].source +
+        '"></div>"');
       }
   }).fail(function() {
     alert('Could not receive data. Please contact admin.');
